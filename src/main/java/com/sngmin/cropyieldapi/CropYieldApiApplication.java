76a7d4c694c500324aa54e46faf0bb6e753991da
lsngmin <lsngminn@gmail.com>
2026-05-06 16:43:09 +0900

    feat: scaffold spring boot project

---
+++ src/main/java/com/sngmin/cropyieldapi/CropYieldApiApplication.java
@@ -0,0 +1,13 @@
+package com.sngmin.cropyieldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropYieldApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropYieldApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sngmin/cropyieldapi/CropYieldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sngmin.cropyieldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropYieldApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
